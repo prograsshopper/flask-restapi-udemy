aa03ed36a2b6a68c4eb691aaf25406253abd5e16
prograsshopper <tjalwled12@gmail.com>
2021-08-26 22:28:01 +0900

    section 4: authentication

--- section4/app.py
+++ section4/app.py
@@ -1,31 +1,56 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'alltherglittersarenotgold'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
-    
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': f'Item {name} is already in here!'}
-        
-        data = request.get_json() # error if header is not proper type
-        item = {'name': name,   'price': data['price']}
-        items.append(item)
-        return item, 201
+        return {'item': next(filter(lambda x: x['name'] == name, items), None)}
 
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists.".format(name)}
+
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # Once again, print something not in the args to verify everything works
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
-        return {'item': items}
-    
+        return {'items': items}
+
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)  # important to mention debug=True
